b51ac442bafed0321f06b110b929a23fe9258abc
usausa <machi.pon@gmail.com>
2021-07-31 18:14:39 +0900

    Fix inspection

--- Smart.Mapper/Mapper/Options/MappingOption.cs
+++ Smart.Mapper/Mapper/Options/MappingOption.cs
@@ -2,7 +2,6 @@ namespace Smart.Mapper.Options
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics.CodeAnalysis;
     using System.Linq;
     using System.Reflection;
 

c03a38ef40e4ff3112d02cf61ada6cb695ca0c3b
usausa <machi.pon@gmail.com>
2021-09-02 15:45:44 +0900

    Update test

--- Smart.Mapper.Tests/Mapper/FieldTest.cs
+++ Smart.Mapper.Tests/Mapper/FieldTest.cs
@@ -38,6 +38,17 @@ namespace Smart.Mapper
             Assert.Equal(-1, mapper.Map<FieldSource, FieldDestination>(new FieldSource { Value = -1 }).Value);
         }
 
+        [Fact]
+        public void MapFieldToFieldManual()
+        {
+            var config = new MapperConfig();
+            config.CreateMap<FieldSource, FieldDestination>()
+                .ForMember(x => x.Value, opt => opt.MapFrom(x => x.Value));
+            using var mapper = config.ToMapper();
+
+            Assert.Equal(-1, mapper.Map<FieldSource, FieldDestination>(new FieldSource { Value = -1 }).Value);
+        }
+
         //--------------------------------------------------------------------------------
         // Data
         //--------------------------------------------------------------------------------
--- Smart.Mapper.Tests/Mapper/MapFromTest.cs
+++ Smart.Mapper.Tests/Mapper/MapFromTest.cs
@@ -1,5 +1,7 @@
 namespace Smart.Mapper
 {
+    using System;
+
     using Smart.Mapper.Functions;
 
     using Xunit;
@@ -108,15 +110,41 @@ namespace Smart.Mapper
             Assert.Equal(3, destination.Value);
         }
 
+        [Fact]
+        public void MapFromByPath2()
+        {
+            var config = new MapperConfig();
+            config.CreateMap<Source, Destination>()
+                .ForMember(d => d.Value, opt => opt.MapFrom(nameof(Source.FieldValue)));
+            using var mapper = config.ToMapper();
+
+            var destination = new Destination();
+            mapper.Map(new Source { FieldValue = -1 }, destination);
+
+            Assert.Equal(-1, destination.Value);
+        }
+
+        [Fact]
+        public void MapFromByPathInvalid()
+        {
+            var config = new MapperConfig();
+            Assert.Throws<ArgumentException>(() =>
+                config.CreateMap<Source, Destination>().ForMember(d => d.Value, opt => opt.MapFrom(string.Empty)));
+        }
+
         //--------------------------------------------------------------------------------
         // Data
         //--------------------------------------------------------------------------------
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Performance")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1051:DoNotDeclareVisibleInstanceFields", Justification = "Ignore")]
         public class Source
         {
             public int Value { get; set; }
 
             public string? StringValue { get; set; }
+
+            public int FieldValue;
         }
 
         public class Destination
